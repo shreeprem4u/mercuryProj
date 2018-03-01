6e79dccb9f96185d26c80b48c3e6bd00f6531425
shreeprem4u <shreeprem4u@gmail.com>
2018-03-01 14:30:04 +0530

    Final Code 01.03.2018

--- mainmercury.py
+++ mainmercury.py
@@ -3,13 +3,8 @@ from time import localtime, strftime
 import mercury
 import pika	#FOR RABBITMQ
 import math
-from datetime import datetime
 import time
 import socket	#TO GET IP ADDRESS OF THE MACHINE
-import json
-
-
-
 
 '''
 READER CONFIGURATION : 
@@ -22,7 +17,7 @@ reader = mercury.Reader("tmr:///dev/ttyACM0")
 #reader = mercury.Reader("tmr://172.17.137.155")
 reader.set_region("IN")
 reader.set_read_plan([1,2], "GEN2")
-#print(reader.ip)	
+	
 
 '''
 ESTABLISHING CONNECTION WITH RABBITMQ :
@@ -33,42 +28,42 @@ IP ADDR - IP ADDR OF RABBITMQ SERVER
 amudavhost - VIRTUAL HOST NAME IN RABBITMQ
 mercury - QUEUE NAME
 '''
-#credentials = pika.PlainCredentials('test', 'test') #username,password for rabbitMq.
-#parameters = pika.ConnectionParameters('172.17.137.155',5672,'amudavhost',credentials)
-credentials = pika.PlainCredentials('amuda', 'amuda2017') #username,password for rabbitMq.
-parameters = pika.ConnectionParameters('172.17.137.160',5672,'amudavhost',credentials)
+credentials = pika.PlainCredentials('test', 'test') #username,password for rabbitMq.
+parameters = pika.ConnectionParameters('172.17.137.155',5672,'amudavhost',credentials)
+#credentials = pika.PlainCredentials('amuda', 'amuda2017') #username,password for rabbitMq.
+#parameters = pika.ConnectionParameters('172.17.137.160',5672,'amudavhost',credentials)
 connection = pika.BlockingConnection(parameters)
 channel = connection.channel()
 channel.queue_declare(queue="mercury")
-dic = {}
-data = []
-def display(data):
-	print(data)
 
+while('true'):
+	dic = {}
+	def display(data):
+		print(data)
 
 
 #FUNCTION DEFINITION
-def send(epc,ant,rssi):
-	dist =math.pow(10,(-52-rssi)/22.0)	#n=1.8, -52 is RSSI at 1 metre distance from antenna
-	timestamp = strftime("%Y-%m-%d %H:%M:%S", localtime())
-	s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM) #These four lines for getting IP address of the machine
-	s.connect(("8.8.8.8", 80))
-	ip = s.getsockname()[0]
-	s.close()
-	print(epc)
-#	dic[epc+","+ip] = ip+","+epc+","+timestamp	#It works perfectly.
-	dic[epc] = ip+","+epc+","+timestamp
+	def process(epc,ant,rssi):
+		dist =math.pow(10,(-52-rssi)/22.0)	#n=1.8, -52 is RSSI at 1 metre distance from antenna at 90 degree
+		timestamp = strftime("%Y-%m-%d %H:%M:%S", localtime())
+		s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM) #These four lines for getting IP address of the machine
+		s.connect(("8.8.8.8", 80))
+		ip = s.getsockname()[0]
+		s.close()
+#		dic[epc+","+ip] = ip+","+epc+","+timestamp	#It works perfectly.
+		dic[epc] = ip+","+epc+","+timestamp
 
 #READING OF TAGS
-reader.start_reading(lambda tag: send(tag.epc, tag.antenna, tag.rssi))
-time.sleep(1.0)	#need to increase the value for more tags to read. This is for one tag
-reader.stop_reading()
-for key, value in dic.iteritems():
-	msg = value
-	parts = msg.split(",")
-	viz = parts[1]+","+"695253.44712"+","+"190577.88868"+","+parts[2]
-	channel.basic_publish(exchange='', routing_key='mercury', body=viz)
-#	print("[x] Sent ")
-	display(viz)
+	reader.start_reading(lambda tag: process(tag.epc, tag.antenna, tag.rssi))
+	time.sleep(1.0)	#need to increase or decrease the value depends on number of tags to read.
+#	reader.stop_reading()
+	count = len(dic)
+	for key, value in dic.iteritems():
+		msg = value
+		parts = msg.split(",")
+		viz = parts[1]+","+"695253.44712"+","+"190577.88868"+","+parts[2]+","+str(count) #epc,(x,y) of antenna,timestamp,number of tags in dictionary
+		channel.basic_publish(exchange='', routing_key='mercury', body=viz)
+		print("[x] Sent ")
+		display(viz)
+	reader.stop_reading()
 
-#print(dic.items())
--- mercrecv_to_db.py
+++ mercrecv_to_db.py
@@ -4,26 +4,12 @@ import datetime
 
 
 
-def dbInsert(c,d,e,f,g):
-	try:
-		conn = psycopg2.connect("dbname='smartbuilding' port='5432' user='amudalab3' host='127.0.0.1' password='amudalab'")
-		#the connection string
-		cur = conn.cursor()		
-		cur.execute("INSERT INTO rawdata (epc,rssi,powerlevel,time,antennaid) VALUES (%s,%s,%s,%s,%s)",(c,d,e,f,g))
-		#the query to insert into rawdata table
-		print("inserted row into rawdata table at ",datetime.datetime.now())
-		conn.commit()
-		cur.close()
-	except:
-		print("Connection Failed in attempt to rawdata")
-
 def dbInsert1(c,d,e,f):
 	try:
 		conn = psycopg2.connect("dbname='smartbuilding' port='5432' user='amudalab3' host='172.17.137.160' password='amudalab'")
 		#the connection string	
 		cur = conn.cursor()		
 		cur.execute("INSERT INTO main_asset (epc,x,y,time,geom) VALUES (%s,%s,%s,%s,ST_SetSRID(ST_MakePoint(%s,%s),4326))",(c,d,e,f,d,e))
-		#the query to insert into main_asset table
 		print("inserted row into main_asset table at ",datetime.datetime.now())
 		conn.commit()
 		cur.close()
@@ -37,22 +23,10 @@ def callback(ch, method, properties, body):
 	data = str(body).split(',')
 	a = len(data)
 	print(a)
-	#the raw data is stored in the table rawdata
-	if a == 5 :
-		dbInsert(data[0],data[2],data[4],data[3],data[1])
-		print(data[0],data[2],data[4],data[3],data[1])
-	#the location data is stored in the table main_asset
-	if a == 4 :
-		if data[2] != 'NaN':
-			dbInsert1(data[0],data[2],data[1],data[3])
-			print(data[0],data[1],data[2],data[3])
-
-#code to consume the data from the rabbitmq server
-#parameters = pika.ConnectionParameters('localhost') 
-#instead of localhost the ip should to used to connect to the rabbitmq server running in the remote machine
+	dbInsert1(data[0],data[2],data[1],data[3]) #needed to insert count received from sender
+	print(data[0],data[1],data[2],data[3])
 
 credentials = pika.PlainCredentials('amuda', 'amuda2017')
-# ip should to used to connect to the rabbitmq server running in the remote machine
 parameters = pika.ConnectionParameters('172.17.137.160',5672,'amudavhost',credentials)
 connection = pika.BlockingConnection(parameters)
 channel = connection.channel()
