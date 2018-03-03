0d066ee2c0a04e0f32bfb60f1b9d50d738aa6ee5
shreeprem4u <shreeprem4u@gmail.com>
2018-03-03 16:56:07 +0530

    03.03.2018

--- queryDB.py
+++ queryDB.py
@@ -1,11 +1,30 @@
 import psycopg2
+from time import localtime, strftime
+
+
+def dbInsert1(c,d,e,f):
+	try:
+		conn = psycopg2.connect("dbname='smartbuilding' port='5432' user='amudalab3' host='172.17.137.160' password='amudalab'")
+		#the connection string	
+		cur = conn.cursor()		
+		cur.execute("INSERT INTO main_asset (epc,x,y,time,geom) VALUES (%s,%s,%s,%s,ST_SetSRID(ST_MakePoint(%s,%s),4326))",(c,d,e,f,d,e))
+		print("inserted row into main_asset table at ",datetime.datetime.now())
+		conn.commit()
+		cur.close()
+	except:
+		print("Connection Failed in attempt to main_asset")
+
+
 
 conn = psycopg2.connect("dbname='smartbuilding' port='5432' user='amudalab3' host='172.17.137.160' password='amudalab'")
 cur = conn.cursor()		
-cur.execute("SELECT * FROM main_asset where epc='0028029C1301230000E6CBD9' ORDER BY time DESC limit 1;")
+
+cur.execute("SELECT DISTINCT ON (epc) epc,x,y,time FROM mercury_asset ORDER BY epc,time DESC;")
 rows = cur.fetchall()
 for row in rows:
-    print(row)
+	time = strftime("%Y-%m-%d %H:%M:%S", localtime())
+#	dbInsert1(row[0],row[1],row[2],time)
+    	print(row)
 conn.commit()
 cur.close()
 
